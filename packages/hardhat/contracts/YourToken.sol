590183bf2e2b781853ac3dfa4d8a4ad0b3df147f
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 19:28:17 -0500

    feat: Checkpoint 1 - Your Token

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -6,5 +6,11 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/5.x/erc20
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {}
+    constructor() ERC20("Gold", "GLD") {
+        _mint(msg.sender, 1000 * (10 ** 18));
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        return super.transfer(to, amount);
+    }
 }
